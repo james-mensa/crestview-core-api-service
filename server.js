07363c17a6e8fe61af04cdc4ac1c7f5a07bc371b
James-mensah <jamesmensahcoded@gmail.com>
2024-10-31 11:54:52 +0000

    google auth fix

--- server.js
+++ server.js
@@ -60,15 +60,15 @@ app.listen(PORT,(er,res)=>{
     }
 })
 
-// app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 
-// if(process.env.NODE_ENV==="production"){
-//     const path=require("path");
-//     app.get("*",(req,res)=>{
-//         res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+if(process.env.NODE_ENV==="production"){
+    const path=require("path");
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"));
 
-//     });
+    });
 
-// }
+}
 
 
